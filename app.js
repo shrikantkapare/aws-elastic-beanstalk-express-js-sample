ebbd20cf69f2ff49ba21f54557dd63b40da98fef
shrikantkapare <44080012+shrikantkapare@users.noreply.github.com>
2022-03-25 12:39:43 +0530

    Update app.js
    minor changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('10X growth...now'));
+app.get('/', (req, res) => res.send('10X growth'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
